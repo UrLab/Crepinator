5ff9479771061a408f7b62a3ea2ebe19b2b70139
iTitou <moiandme@gmail.com>
2016-03-01 17:12:46 +0100

    [add] Small CLI tool

---
+++ printerbridge.py
@@ -0,0 +1,34 @@
+from serial import Serial
+from glob import glob
+
+
+def repl(port):
+    with Serial(port, 250000) as P:
+        P.write("\r\n")
+        l = ""
+        while l.strip() != 'echo:Unknown command: ""':
+            l = P.readline()
+
+        while True:
+            line = raw_input(">> ")
+            i = line.find(';')
+            if i >= 0:
+                line = line[:i]
+            line = line.strip()
+            if line:
+                P.write(line + "\r\n")
+                l = P.readline().strip()
+                print "--->", l
+                assert l == "ok", l
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    ports = glob("/dev/ttyACM*")
+    if len(argv) > 1:
+        repl(argv[1])
+    elif len(ports) == 1:
+        repl(ports[0])
+    else:
+        print("Could not determine the printer port")
